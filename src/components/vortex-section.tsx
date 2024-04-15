2c97d58d1d751cf4ed3a34cdf058f730a7e1d815
Vinicius Assis <vinicius.coassis@gmail.com>
2024-04-15 12:10:47 -0300

    fix: responsive for vortex section

--- src/components/vortex-section.tsx
+++ src/components/vortex-section.tsx
@@ -2,24 +2,23 @@ import Image from "next/image";
 
 export default function VortexSection() {
   return (
-    <section className="grid grid-cols-2 bg-secondary inset-0 min-h-screen p-11 md:p-32">
-      <div
-        className=" p-11 mx-auto
-      "
-      >
+    <section className="flex flex-col lg:grid lg:grid-cols-2 bg-secondary inset-0 min-h-screen p-11 mx-a md:p-6 lg:p-32">
+      <div className="order-1 lg:order-1">
         <h1 className="text-4xl font-bold text-center text-secondary-foreground">
           Vortex
         </h1>
         <h2 className="text-2xl text-center text-secondary-foreground">$TEX</h2>
-        <Image
-          src="/logo.png"
-          alt="Vortex"
-          width={800}
-          height={800}
-          className="rounded-full"
-        />
+        <div className="flex justify-center">
+          <Image
+            src="/logo.png"
+            alt="Vortex"
+            width={800}
+            height={800}
+            className="rounded-full"
+          />
+        </div>
       </div>
-      <div className=" p-11 mx-auto text-center flex flex-col gap-4 justify-center text-2xl uppercase">
+      <div className="order-2 lg:order-2 p-0 mx-auto text-center flex flex-col gap-4 lg:justify-normal xl:justify-center lg:text-sm  text-2xl xl:text-2xl uppercase">
         <h1 className="text-4xl font-bold text-center text-secondary-foreground">
           Tokenomics
         </h1>
@@ -28,7 +27,6 @@ export default function VortexSection() {
         <p>No Presale</p>
         <p>No Taxes</p>
         <p>No BS</p>
-
         <p>0x16e17Bf68F99DA63326677431efEB1F6FfD46eDe</p>
       </div>
     </section>
