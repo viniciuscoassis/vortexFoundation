491c06653a9dc1d825ac5a5ba69649a9479e1511
Vinicius Assis <vinicius.coassis@gmail.com>
2024-10-03 15:20:20 -0300

    feat: hidden dialog

--- src/app/(front)/oracles/page.tsx
+++ src/app/(front)/oracles/page.tsx
@@ -10,6 +10,18 @@ import {
 } from "@/components/ui/card";
 import useOraclesStore from "@/store/oracles";
 import { Progress } from "@/components/ui/progress";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export default function Page() {
   const { oracles, loadingProgress, setOracles } = useOraclesStore();
@@ -39,7 +51,7 @@ export default function Page() {
           Oracles: Embark On A Cosmic Journey 🌀
         </h1>
       </header>
-      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-2 min-h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-2 min-h-screen">
         {oracles.map((oracle, index) => (
           <Card key={index}>
             <CardHeader className=" h-36">
@@ -56,9 +68,44 @@ export default function Page() {
                 height={400}
                 className="rounded-lg"
               />
-              
             </CardContent>
-            
+            {/* <Dialog>
+              <DialogTrigger className="w-full">
+                <Button className="w-full" variant="secondary">
+                  Details
+                </Button>
+              </DialogTrigger>
+                <DialogContent className="sm:min-w-[600] sm:min-h-0 md:min-w-[900] lg:min-w-[1000] xl:min-w-[1200px] 2xl:min-w-[1500px]">
+                <DialogHeader className="grid-cols-2">
+                    <DialogTitle>{oracle.name}</DialogTitle>
+                    <DialogDescription>{oracle.description}</DialogDescription>
+                  </DialogHeader>
+                <div className="grid grid-cols-3 gap-4 py-4">
+                  <div className="col-span-1">
+                    <Image
+                      src={oracle.image.replace(
+                        "ipfs://",
+                        "https://ipfs.io/ipfs/"
+                      )}
+                      alt={oracle.name}
+                      width={800}
+                      height={800}
+                      className="rounded-lg"
+                    />
+                  </div>
+                  <div className="col-span-2">
+                    <h1 className="text-2xl font-bold text-center text-secondary-foreground">
+                        Tokenomics
+                        </h1>
+                  </div>
+              
+                </div>
+
+                <DialogFooter>
+                  <Button type="submit">Check on Paintswap</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog> */}
           </Card>
         ))}
       </div>
